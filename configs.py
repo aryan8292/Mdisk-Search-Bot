5fa775ce0601f42f14dd2db98fa43769c62d4b2c
aryan8292 <94910943+aryan8292@users.noreply.github.com>
2023-02-24 12:11:55 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,21 +1,24 @@
-# (c) @RoyalKrrishna
+# (c) @AM_ROBOTS
 
 import os
 
 
 class Config(object):
     API_ID = int(os.environ.get("API_ID", 12345))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_HASH = os.environ.get("API_HASH", "c33b2f280810fc2f60a6387a4c4217f2")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5015404755:AAE58laq-OGoqxkKth7TjVzyKYBTa4zs7oc")
     BOT_SESSION_NAME = os.environ.get("BOT_SESSION_NAME", "MdiskSearchBot")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "")
-    CHANNEL_ID = int(os.environ.get("CHANNEL_ID", -100))
-    BOT_USERNAME = os.environ.get("BOT_USERNAME")
-    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
+    CHANNEL_ID = int(os.environ.get("CHANNEL_ID", -1001711211283))
+    BOT_USERNAME = os.environ.get("Arymovies_bot")
+    BOT_OWNER = int(os.environ.get("5079629749")
+    DATABASE_URL = os.environ.get("DATABASE_URL", ("mongodb+srv://Aryan6969:Aryan6969@cluster0.krhmwhe.mongodb.net/?retryWrites=true&w=majority")
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
     ABOUT_BOT_TEXT = """<b>This is Mdisk Search Bot.
-
-🤖 My Name: <a href='https://t.me/cyniteofficial'>Mdisk Search Robot</a>
+    
+    
+    
+🤖 My Name: <a href='https://t.me/Official_Movies_Group'>Mdisk Search Robot</a>
 
 📝 Language : <a href='https://www.python.org'> Python V3</a>
 
@@ -23,10 +26,10 @@ class Config(object):
 
 📡 Server: <a href='https://heroku.com'>Heroku</a>
 
-👨‍💻 Created By: <a href='https://t.me/cyniteofficial'>Cynite</a></b>
+👨‍💻 Created By: <a href='https://t.me/Am_RoBots'>ᎯℕUℛᎯᎶ</a></b>
 """
 
-    ABOUT_HELP_TEXT = """<b>👨‍💻 Developer : <a href='https://t.me/cyniteofficial'>Click Me</a>
+    ABOUT_HELP_TEXT = """<b>👨‍💻 Developer : <a href='https://t.me/Am_RoBots'>ᎯℕUℛᎯᎶ</a>
 
 If You Want Your Own Bot Like This Then You Can Contact Our Developer.</b>
 """
@@ -38,7 +41,7 @@ I'm Mdisk Search Robot.🤖</a>
 
 I Can Search 🔍 What You Want❗
 
-<a>Made With ❤ By @Cyniteofficial</a></b>
+<a>Made With ❤ By @Am_RoBots</a></b>
 """
 
 
@@ -49,7 +52,6 @@ I'm Mdisk Search Robot.🤖</a>
 
 I Can Search 🔍 What You Want❗
 
-<a>Made With ❤ By @Cyniteofficial</a></b>
+<a>Made With ❤ By @Am_RoBots</a></b>
 """
-
 
